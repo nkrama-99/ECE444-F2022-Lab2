b7c92761d8e145aa2eaf89c1297cb79626b05980
Ramakrishna Natarajan <nkrama99@gmail.com>
2022-09-21 14:00:46 -0400

    activity3 - implement home page

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from ensurepip import bootstrap
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
